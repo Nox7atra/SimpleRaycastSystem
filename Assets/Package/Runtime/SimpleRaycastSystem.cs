be0ef816f3a230a5d1738c490cb77f3827a06768
Grygory <dyadichenkoga@foxsys.pro>
2021-10-20 12:30:23 +0300

    add layers support

--- Assets/Package/Runtime/SimpleRaycastSystem.cs
+++ Assets/Package/Runtime/SimpleRaycastSystem.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public static class SimpleRaycastSystem
 {
     private static List<SimpleRayCastable> _SimpleRayCastables = new List<SimpleRayCastable>();
     private static List<SimpleRaycastHit> _CurrentHitsBuffer = new List<SimpleRaycastHit>();
+
     public static bool Raycast (Ray ray, out SimpleRaycastHit hit)
     {
+        return Raycast(ray, out hit, LayerMask.NameToLayer("Default"));
+    }
+    public static bool Raycast (Ray ray, out SimpleRaycastHit hit, LayerMask mask)
+    {
         IEnumerable<SimpleRaycastHit> hits;
-        if (RaycastAll(ray, out hits))
+        if (RaycastAll(ray, out hits, mask))
         {
             hit = hits.First();
             return true;
@@ -20,6 +26,10 @@ public static class SimpleRaycastSystem
         }
     }
     public static bool RaycastAll (Ray ray, out IEnumerable<SimpleRaycastHit> hits)
+    {
+        return RaycastAll(ray, out hits, LayerMask.NameToLayer("Default"));
+    }
+    public static bool RaycastAll (Ray ray, out IEnumerable<SimpleRaycastHit> hits, LayerMask mask)
     {
         _CurrentHitsBuffer.Clear();
         foreach (var simpleRayCastable in _SimpleRayCastables)
@@ -34,7 +44,7 @@ public static class SimpleRaycastSystem
             hits = null;
             return false;
         }
-        hits = _CurrentHitsBuffer.OrderBy( hit => hit.distance);
+        hits = _CurrentHitsBuffer.OrderBy(hit => hit.distance).Where(x=> mask == (mask | (1 << x.transform.gameObject.layer)));
         return true;
     }
     public static void Register (SimpleRayCastable simpleRayCastable)
